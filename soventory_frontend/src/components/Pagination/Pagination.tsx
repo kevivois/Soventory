9009001b65e2920b3955236eb900bb7c5eb7858e
Kevin Voisin <kevin.voisin187@gmail.com>
2023-01-15 17:14:33 +0100

    added row in nav

--- soventory_frontend/src/components/Pagination/Pagination.tsx
+++ soventory_frontend/src/components/Pagination/Pagination.tsx
@@ -1,5 +1,7 @@
 import React, { useEffect, useState } from 'react'
 import { Button } from '@mui/material'
+import NavigateNextIcon from '@mui/icons-material/NavigateNext';
+import NavigateBeforeIcon from '@mui/icons-material/NavigateBefore';
 export default function Pagination(props:{onPageChange:(page:number) => void,enabled:boolean,page:number,maxPage:number}){
 
     function onPrevious(){
@@ -10,11 +12,11 @@ export default function Pagination(props:{onPageChange:(page:number) => void,ena
     }
     if(props.enabled){
         return (<div className="pagination">
-            <Button className="pageButton" sx={{color:"#f13dbe"}} onClick={onPrevious} disabled={props.page === 1 || !props.enabled}>Précedent</Button>
+            <Button className="pageButton" sx={{color:"#f13dbe"}} onClick={onPrevious} disabled={props.page === 1 || !props.enabled}>< NavigateBeforeIcon /></Button>
             <span>{props.page}</span>
             <span> sur </span>
             <span>{isFinite(props.maxPage) ? props.maxPage : 0 }</span>
-            <Button className="pageButton" sx={{color:"#f13dbe"}} onClick={onNext} disabled={props.page >= props.maxPage|| !props.enabled}>Suivant</Button>
+            <Button className="pageButton" sx={{color:"#f13dbe"}} onClick={onNext} disabled={props.page >= props.maxPage|| !props.enabled}>< NavigateNextIcon /></Button>
         </div>)
         }
     else{
