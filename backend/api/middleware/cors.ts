1153ed11269a63c02d85c5c944ddb73652de80cd
Kevin Voisin <kevin.voisin187@gmail.com>
2023-03-15 12:59:49 +0100

    production settings

--- backend/api/middleware/cors.ts
+++ backend/api/middleware/cors.ts
@@ -1,6 +1,6 @@
 import getIp from "../../IP";
 export async function handleCors(req:any, res:any, next:any) {
- res.header('Access-Control-Allow-Origin','http://soventory.info')
+ res.header('Access-Control-Allow-Origin','http://soventory')
  res.header('Access-Control-Allow-Credentials',true)
 
  res.header('Access-Control-Allow-Headers','Origin, X-Requested-With, Content-Type, Accept, Authorization')
--- backend/api/setupApi.ts
+++ backend/api/setupApi.ts
@@ -22,7 +22,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
 app.use(cors({
-    origin:'http://soventory.info',
+    origin:'http://soventory',
     credentials:true
 }))
 app.use(handleCors)
