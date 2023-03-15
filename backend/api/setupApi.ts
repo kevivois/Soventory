7cf8adcf9d13e2cb941b4a34372457b37740f192
Kevin Voisin <kevin.voisin187@gmail.com>
2023-03-15 15:28:52 +0100

    restore current dev version

--- backend/api/setupApi.ts
+++ backend/api/setupApi.ts
@@ -22,7 +22,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
 app.use(cors({
-    origin:'http://soventory.info',
+    origin:'http://localhost:3000',
     credentials:true
 }))
 app.use(handleCors)
